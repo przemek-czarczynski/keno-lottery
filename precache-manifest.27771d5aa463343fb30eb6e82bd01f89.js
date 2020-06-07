383002be22eeabf5f05d530045b747098575322d
unknown <przemek.73@gmail.com>
2020-06-07 12:20:54 +0200

    Updates

---
+++ precache-manifest.27771d5aa463343fb30eb6e82bd01f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2ec8e3596545babc3834ed0662a7ed",
+    "url": "/keno-lottery/index.html"
+  },
+  {
+    "revision": "c9fc260b1bc25c2d4ac1",
+    "url": "/keno-lottery/static/css/main.b8ee11be.chunk.css"
+  },
+  {
+    "revision": "5a51a5fda6ad641dcdc4",
+    "url": "/keno-lottery/static/js/2.7b14daae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keno-lottery/static/js/2.7b14daae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fc260b1bc25c2d4ac1",
+    "url": "/keno-lottery/static/js/main.b43ec4ba.chunk.js"
+  },
+  {
+    "revision": "fdb86957bfa01c62d24b",
+    "url": "/keno-lottery/static/js/runtime-main.69fa63e5.js"
+  }
+]);
